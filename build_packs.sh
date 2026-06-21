c3d3ab8e256c80b5213628bd9896cc203757bf7c
BubbleFish <daforsastudia@gmail.com>
2026-06-21 17:41:39 +0300

    chore: Bump version and fix the resource pack overlay

--- build_packs.sh
+++ build_packs.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 vintage build --delete-unused-libs --force-stringify $*
+
+# This is a workaround, due to vintage (the build tool) not supporting overlays yet.
 cp -r ./overlays/* ./build/data_pack/.
+jq --tab 'del(.overlays)' ./build/resource_pack/pack.mcmeta >/tmp/pack.mcmeta
+mv /tmp/pack.mcmeta ./build/resource_pack/pack.mcmeta
